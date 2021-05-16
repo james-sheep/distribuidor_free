40b60d472679d80637be4662a84185433c1d5384
carneiro <carneiro@pop-os.localdomain>
2021-05-16 18:54:49 -0300

    make it better

--- principal.py
+++ principal.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from flask import Flask
-
+from datetime import date, datetime
 
 app = Flask(__name__)
 
@@ -24,32 +24,41 @@ def index():
 
 @app.route('/notificar', methods=['GET','POST'])
 def notificar():
-        if request.method == "POST":
-
-         emails = request.form["listaDeEmail"]
-         responsavel = request.form["coordenador"]
-         lista_email = emails.split(",")
-         coordenadorEmail = request.form["coordenadorEmail"]
-         pessoas = request.form["listaDePessoas"]
-         lista_pessoas = pessoas.split(",")
-         
-         tarefas = request.form["listaDeTarefas"]
-         lista_tarefas = tarefas.split(",")
-         numeroTarefas = len(lista_tarefas)
-         numero_pessoas = len(lista_pessoas)
-         print(lista_email)
-         print(lista_pessoas)
-         print(lista_tarefas)
         
-        return render_template('resultado.html', responsavel=responsavel,
+        if request.method == "POST":
+                
+                data_atual = date.today()
+                hora_atual = datetime.now()
+                data_em_texto = f"{data_atual.day}/{data_atual.month}/{data_atual.year}"               
+                hora_em_texto = hora_atual.strftime('%H:%M')
+                
+                
+                emails = request.form["listaDeEmail"]
+                responsavel = request.form["coordenador"]
+                lista_email = emails.split(",")
+                coordenadorEmail = request.form["coordenadorEmail"]
+                pessoas = request.form["listaDePessoas"]
+                lista_pessoas = pessoas.split(",")
+
+                tarefas = request.form["listaDeTarefas"]
+                lista_tarefas = tarefas.split(",")
+                numeroTarefas = len(lista_tarefas)
+                numero_pessoas = len(lista_pessoas)
+                print(lista_email)
+                print(lista_pessoas)
+                print(lista_tarefas)
+
+                return render_template('resultado.html', responsavel=responsavel,
                                                 lista_email = lista_email, 
                                                 lista_tarefas=lista_tarefas, 
                                                 lista_pessoas = lista_pessoas,
                                                 numeroTarefas = numeroTarefas,
                                                 numero_pessoas = numero_pessoas,
-                                                coordenadorEmail = coordenadorEmail)
+                                                coordenadorEmail = coordenadorEmail,
+                                                data = data_em_texto,
+                                                hora = hora_em_texto)
 
-        
+
 
 
 
